6541b2217e195496e6ddc046166da1cd8b12fd2b
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2024-02-02 19:07:25 +0530

    sidebar and invite function

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -14,9 +14,9 @@ export const NavigationAction = () => {
           onClick={() => onOpen('createServer')}
           className="group flex items-center"
         >
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-cyan-600">
+          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-sky-800">
             <Plus
-              className="group-hover:text-white transition text-cyan-600"
+              className="group-hover:text-white transition text-sky-600"
               size={25}
             />
           </div>
